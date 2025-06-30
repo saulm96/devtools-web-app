ab9609a6fd4ee362f35df38fef78a23fc24e8675
saulm96 <saulmorahernandez96@gmail.com>
2025-06-30 19:26:40 +0200

    Stablished the user for the sequelize-typescript model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,20 +1,78 @@
-import { Table, Column, Model, PrimaryKey, Default, DataType, IsEmail, Unique } from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  Default,
+  DataType,
+  IsEmail,
+  BeforeSave,
+  Length,
+} from "sequelize-typescript";
+
+import bcrypt from "bcrypt";
 
 @Table({
-  tableName: 'users',
+  tableName: "users",
   timestamps: true,
+  paranoid: true,
 })
 export class User extends Model {
+  //id
   @PrimaryKey
   @Default(DataType.UUIDV4)
   @Column(DataType.UUID)
   public declare id: string;
 
+  //email
+  @IsEmail
+  @Column({
+    allowNull: false,
+    unique: true,
+    type: DataType.STRING(50),
+  })
+  public email!: string;
+
+  //username
+  @Length({ min: 3, max: 20 })
+  @Column({
+    allowNull: false,
+    unique: true,
+    set(value: string) {
+      this.setDataValue("username", value.toLowerCase());
+    },
+  })
+  public username!: string;
+
+  //first name
   @Column({ allowNull: false })
   public firstName!: string;
 
-  @Unique
-  @IsEmail 
+  //last name
+  @Column({ allowNull: true })
+  public lastName!: string;
+
+  //password
+  @Length({ min: 8 })
   @Column({ allowNull: false })
-  public email!: string;
+  public password!: string;
+
+  //HOOKS
+  @BeforeSave
+  static async hashPassword(user: User) {
+    if (user.changed("password")) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+    }
+  }
+
+  public async validatePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
+
+  public toJSON() {
+    const values = { ...this.get() };
+    delete values.password;
+    return values;
+  }
 }
